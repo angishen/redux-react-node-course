d516c471b077dbb5a473818720da189f784f16dc
Angela Shen <angelashen01@gmail.com>
2018-08-20 15:57:15 -0700

    configure redux and add boilerplate

--- services/passport.js
+++ services/passport.js
@@ -33,14 +33,13 @@ passport.use(
       const existingUser = await User.findOne({ googleId: profile.id });
       if (existingUser) {
         // we already have a record w/ the given profile id
-        done(null, existingUser);
-      } else {
-        // we don't have a user record, make a new record
-        // create a new User model instance
-        // .save() persists model instance to the db
-        const user = await new User({ googleId: profile.id }).save();
-        done(null, user);
+        return done(null, existingUser);
       }
+      // we don't have a user record, make a new record
+      // create a new User model instance
+      // .save() persists model instance to the db
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
