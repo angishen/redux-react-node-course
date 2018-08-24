93593a9198c1dc3b429630c9a7af8c5a3dde5a5c
Angela Shen <angelashen01@gmail.com>
2018-08-24 14:24:18 -0700

    finish new surveyform

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
 
+
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  //reduxForm must be assigned to "form" key
+  form: reduxForm
 });
