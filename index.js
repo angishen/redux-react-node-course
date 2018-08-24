c246131ae441e0a651700f6d0ec1570b36f30559
Angela Shen <angelashen01@gmail.com>
2018-08-24 11:15:11 -0700

    finish surveys backend

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -30,6 +31,7 @@ app.use(passport.session());
 // to calling authRoutes(app)
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets like main.js and main.css
