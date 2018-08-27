bbd0657904a663243b1bf60e135362e0c847b861
Angela Shen <angelashen01@gmail.com>
2018-08-26 22:46:38 -0700

    finish find and update survey instance in db

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,14 +10,14 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
     const p = new Path('/api/surveys/:surveyId/:choice');
 
-    const events = _.chain(req.body)
+    _.chain(req.body)
       .map(({ url, email }) => {
         // match will be object or null if path with above pattern couldn't be extracted
         const match = p.test(new URL(url).pathname);
@@ -31,9 +31,29 @@ module.exports = app => {
       })
       .compact()
       .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              // finds only the subdoc instance that matches the criteria
+              $elemMatch: { email, responded: false }
+            }
+          },
+          {
+            // $inc - mongo operator
+            // find the choice property (yes or know) and increment (inc) by one
+            // [choice] - es6 key interpolation
+            $inc: { [choice]: 1 },
+            // set responded property inside the recipients subdoc to true
+            // $ index used in place of index from elemMatch
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
+          }
+          // .exec() executes query
+        ).exec();
+      })
       .value();
-
-    console.log(events);
 
     res.send({});
   });
